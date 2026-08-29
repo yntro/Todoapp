04942f9c17f1e00da1c1a326c39909a4f7df5745
yntro <milaknis.mar@gmail.com>
2026-08-29 12:57:40 +0300

    first commit

---
+++ todo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('todo/', include('todoapp.urls')),
+]
